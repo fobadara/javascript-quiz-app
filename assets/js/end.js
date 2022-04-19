51a8a9ab7a0226bf0499b3f7f58020f7fc4273b0
Ganiat Badara <ganiyatbadara@gmail.com>
2022-04-19 17:14:27 +0000

    Save High Scores in Local Storage

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -12,4 +16,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log("clicked on the save button!");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 };
